0e76acb58d3c6181825f2b3409953675d5ba48f5
Alyssa <alyssaw90@gmail.com>
2014-12-08 15:44:05 -0800

    need back button and delete button

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var express = require('express'),
 	bodyParser = require ('body-parser'),
 	ejs = require('ejs')
 	app = express();
-var request = require('request')
+
+var request = require('request');
+var models = require("./models");
+var db = require ("./models/index.js");
+
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-	res.render("index")
+	res.render('home');
 })
 
 app.get("/search", function(req, res){
@@ -20,12 +25,28 @@ app.get("/search", function(req, res){
 		if (!error && response.statusCode == 200) {
 			var info = JSON.parse(body);
 			// res.send(info)
-			res.render("search", info)
+			res.render("search", {movies: info});
 		} else {
 			console.log("ERRROOOR");
 		}
 	})
 })
+
+app.get("/watchlist", function(req, res){
+	var added = req.query.added || false;
+	db.Movie.findAll().then(function(data){
+		// res.send(data)
+		res.render("watchlist", {'list':data})
+	})
+});
+
+
+app.post("/watchlist", function(req, res){
+	db.Movie.findOrCreate({where: {imdb_code:req.body.imdb_code, movie_title:req.body.movie_title, year:req.body.year}}).done(function(err, data, created){
+		res.redirect("watchlist")
+	})
+});	
+
 
 app.get("/:imdbID", function(req, res){
 	var id = req.params.imdbID
@@ -42,5 +63,5 @@ app.get("/:imdbID", function(req, res){
 	})
 })
 
-
 app.listen(3000);
+
